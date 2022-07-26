67642a2ea9130f75c60fc0635146d4f7778fefac
zskio <vwind5224@gmail.com>
2022-07-26 21:17:35 +0400

    navbar items is completed

--- src/components/Navbar/FilterByCreated.jsx
+++ src/components/Navbar/FilterByCreated.jsx
@@ -6,24 +6,23 @@ const FilterByCreated = () => {
   // Context
   const { data, setCurrentData } = useContext(DataContext);
 
+  // Setting hook for taking input text "from and before"
+  const [from, setFrom] = useState("");
+  const [before, setBefore] = useState("");
+
   // Open & Close author list
   const [open, setOpen] = useState(false);
 
   // Getting filtered array by search field
   const getFilteredData = () => {
-    const newFilteredArray = data.filter((painting) => {
-      return painting.location.includes(location);
+    const newFilteredArray = data.filter(({ created }) => {
+      return created >= from && created <= before;
     });
-    setCurrentData(newFilteredArray);
+    setCurrentData(from && before === "" ? data : newFilteredArray);
   };
   useEffect(() => {
     getFilteredData();
-  }, [location]);
-
-  // On click handler for changing current author
-  const onClickHandler = (e) => {
-    setOpen(!open);
-  };
+  }, [from, before]);
 
   return (
     <div className={`navbar-item ${open ? " opened" : ""}`}>
@@ -37,20 +36,20 @@ const FilterByCreated = () => {
       </div>
 
       {open ? (
-        <div className="drop-down-list">
-          <ul>
-            {data.map((painting) => {
-              return (
-                <li
-                  key={painting.id}
-                  className="drop-down-item"
-                  onClick={(e) => onClickHandler(e)}
-                >
-                  {painting.location}
-                </li>
-              );
-            })}
-          </ul>
+        <div className="created">
+          <form>
+            <input
+              type="text"
+              placeholder="from"
+              onChange={(e) => setFrom(e.target.value)}
+            />
+            <div className="divider" />
+            <input
+              type="text"
+              placeholder="before"
+              onChange={(e) => setBefore(e.target.value)}
+            />
+          </form>
         </div>
       ) : null}
     </div>
