fa5856ec06a13fd6a626575a53f7267b6d216c4e
zskio <vwind5224@gmail.com>
2022-08-05 09:13:28 +0400

    separated to two different components(paintings to paintingsList and paintingItem)

---
+++ src/components/PaintingItem/PaintingItem.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./styles.sass";
+
+const PaintingItem = ({ paintingItem }) => {
+  const { id, name, author, location, created, imageUrl } = paintingItem;
+  return (
+    <div className="painting-item" key={id}>
+      <img src={`${imageUrl}`} alt="" />
+      <div className="painting-description-container">
+        <h2 className="painting-title">{name}</h2>
+        <div className="painting-subtitle">
+          <p>
+            <span>Author:</span> {author}
+          </p>
+          <p>
+            <span>Created:</span> {created}
+          </p>
+          <p>
+            <span>Location:</span> {location}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PaintingItem;
---
+++ src/components/PaintingsList/PaintingsList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.sass";
+import DataContext from "../../contexts/DataContext";
+import { useContext } from "react";
+import { PAINTING_PER_PAGE } from "../../utils/constants";
+import PaintingItem from "../PaintingItem";
+
+const PaintingsList = ({ currentData }) => {
+  // Context
+  const { page } = useContext(DataContext);
+  const startIndex = (page - 1) * PAINTING_PER_PAGE;
+  const selectedPaintings = currentData.slice(
+    startIndex,
+    startIndex + PAINTING_PER_PAGE
+  );
+
+  return (
+    <section className="paintings-list">
+      {selectedPaintings.map((paintingItem) => {
+        return <PaintingItem paintingItem={paintingItem} />;
+      })}
+    </section>
+  );
+};
+
+export default PaintingsList;
